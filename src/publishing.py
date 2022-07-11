6f4585ff82886cecf8756c41bceaba4cab8cc704
Felix <felixludos.info@gmail.com>
2022-07-12 01:06:38 +0200

    prep notion sync

--- src/publishing.py
+++ src/publishing.py
@@ -1,9 +1,107 @@
+from typing import Union, List, Dict, Callable, Tuple, Optional
+from functools import lru_cache
+import omnifig as fig
+
+from .util import Script_Manager
+from .auth import ZoteroProcess
 
 
+class Extractor:
+	
+	class ExtractionError(Exception):
+		pass
+	
+	def __call__(self, item, get_children):
+		raise NotImplementedError
+		
+
+class TitleExtractor:
+	def __call__(self, item, get_children):
+		return item['data']['title']
 
 
+class AuthorExtractor:
+	def __call__(self, item, get_children):
+		
+		names = []
+		for creator in item['data']['creators']:
+			if 'name' in creator:
+				names.append(creator['name'])
+			elif 'lastName' in creator and 'firstName' in creator:
+				names.append(f'{creator["firstName"]} {creator["lastName"]}')
+			elif 'lastName' in creator:
+				names.append(creator['lastName'])
+			elif 'firstName' in creator:
+				names.append(creator['firstName'])
+			else:
+				raise self.ExtractionError(f'No name found for creator {creator}')
+		
+		return '\n'.join(names)
 
 
+class Publisher(fig.Configurable):
+	def __init__(self, A, **kwargs):
+		super().__init__(A, **kwargs)
+		self.notion_link_attachment = A.pull('notion-link-attachment', 'Notion')
+		
+		self.extractors: Dict[Extractor] = A.pull('extractors', {})
+
+
+	@property
+	def ident(self):
+		return 'default'
+
+
+	def extract(self, item, get_children, manager):
+		# manage Notion attachment (and check if a page already exists)
+		
+		# extract data from item
+		data = {name: extractor(item, get_children) for name, extractor in self.extractors.items()}
+		
+		return data
+	
+	
+	def publish(self):
+		# Notion API request/s
+		pass
+
+
+@fig.Script('sync-notion', description='Sync Zotero items with Notion database.')
+def sync_notion(A):
+	A.push('manager._type', 'zotero-manager', overwrite=False, silent=True)
+	A.push('manager.pbar-desc', 'Sync with Notion', overwrite=False, silent=True)
+	manager: Script_Manager = A.pull('manager')
+	
+	publisher: Publisher = A.pull('publisher')
+	
+	A.push('brand-tag', f'notion:{publisher.ident}', overwrite=False, silent=True)
+	A.push('zotero._type', 'zotero', overwrite=False, silent=True)
+	zot: ZoteroProcess = A.pull('zotero')
+	
+	zot_query = A.pull('zotero-query', {})
+	
+	manager.preamble(zot=zot)
+	
+	todo = zot.top(**zot_query)
+	manager.log(f'Found {len(todo)} new items to process.')
+	
+	for item in manager.iterate(todo):
+		@lru_cache
+		def get_children(**kwargs):
+			return zot.children(item['key'], **kwargs)
+		try:
+			rawinfo = publisher.extract(item, get_children=get_children, manager=manager)
+		except Exception as e:
+			manager.log_error(e, item=item)
+		else:
+			if rawinfo is None:
+				manager.add_failed(item)
+			else:
+				manager.add_update(item, msg=f'Syncing {len(rawinfo)} items: {", ".join(rawinfo)}')
+	
+	if manager.is_real_run:
+		publisher.publish()
+	return manager.finish()
 
 
 
